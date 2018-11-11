19ba05c823b89168878b99c80c9e8f9fb1f4f957
Viktor Kozhin <viyy26@gmail.com>
2018-11-11 18:36:05 +0300

    Дз ко 2 уроку

---
+++ Lesson2/Lesson2/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson2.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
